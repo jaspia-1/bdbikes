537ccce60a12e73b0ff6a68ca0a008fb6a4840a4
jaspia-1 <aspia2001.icloud@gmail.com>
2022-11-28 19:24:38 +0600

    catagory updating

--- src/Pages/Home/catagory/Catagory.js
+++ src/Pages/Home/catagory/Catagory.js
@@ -7,13 +7,13 @@ import suzuki from '../../../assets/catagory/suzuki.png'
 
 const Catagory = () => {
     return (
-        <div className='container mx-auto my-9'>
-            <div className='text-center'>
-
-                <h1 className="text-5xl my-3 font-bold bebas capitalize">We allow only these brands to buy and sell</h1>
+        <div className='container mx-auto mt-16 '>
+            <div className=''>
+                <h1 className='text-center  text-5xl bebas'>Brands</h1>
+                <h1 className="text-3xl my-3 font-bold roboto capitalize">We allow only these brands to buy and sell</h1>
                 <p className='text-slate-600'>Select any catagory to explore</p>
             </div>
-            <div className='grid grid-cols-4 items-center'>
+            <div className='grid grid-cols-4 items-center mb-10'>
                 <Link className='hover-brand mx-auto '> <img src={ktm} alt="" /></Link>
                 <Link className='hover-brand mx-auto '> <img src={kawasaki} alt="" /></Link>
                 <Link className='hover-brand mx-auto '> <img src={honda} alt="" /></Link>
