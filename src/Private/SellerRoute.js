6c8b567aa4ef47015a5f7b312bf11d79d33b5553
jaspia-1 <aspia2001.icloud@gmail.com>
2022-11-29 12:45:56 +0600

    verification of admin and seller and routing

---
+++ src/Private/SellerRoute.js
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { AuthContext } from '../context/AuthProvider';
+import useSeller from '../hooks/useSeller';
+import Spinner from '../Pages/Shared/Spinner/Spinner';
+
+const SellerRoute = ({ children }) => {
+    const { user, loading } = useContext(AuthContext);
+    const [isSeller, sellerLoading] = useSeller(user?.email)
+
+    const location = useLocation();
+    if (user && isSeller) {
+        return children;
+    }
+    if (loading || sellerLoading) {
+        return <Spinner></Spinner>
+    }
+
+    return <Navigate to='/login' state={{ from: location }} replace></Navigate>
+};
+
+export default SellerRoute;
--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import Main from '../Layout/Main';
+import AddProduct from '../Pages/Dashboards/AddProduct/AddProduct';
+
 import Dashbaords from '../Pages/Dashboards/Dashbaords';
+import MyOrders from '../Pages/Dashboards/MyOrders/MyOrders';
+import MyProducts from '../Pages/Dashboards/MyProducts/MyProducts';
+import MyWishList from '../Pages/Dashboards/MyWishList/MyWishList';
 import WelcomeDashboard from '../Pages/Dashboards/Welcome/WelcomeDashboard';
 import Home from '../Pages/Home/Home';
 import Login from '../Pages/Login/Login';
 import SignUp from '../Pages/SignUp/SignUp';
 import Private from '../Private/Private';
+import SellerRoute from '../Private/SellerRoute';
 
 const router = createBrowserRouter([
     {
@@ -35,7 +41,23 @@ const router = createBrowserRouter([
             {
                 path: '/dashboard',
                 element: <WelcomeDashboard></WelcomeDashboard>
-            }
+            },
+            {
+                path: '/dashboard/addaproduct',
+                element: <SellerRoute><AddProduct></AddProduct></SellerRoute>
+            },
+            {
+                path: '/dashboard/myproducts',
+                element: <SellerRoute><MyProducts></MyProducts> </SellerRoute>
+            },
+            {
+                path: '/dashboard/mywishlist',
+                element: <Private><MyWishList></MyWishList> </Private>
+            },
+            {
+                path: '/dashboard/myorders',
+                element: <Private><MyOrders></MyOrders> </Private>
+            },
         ]
     }
 ]);
